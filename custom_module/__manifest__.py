b93ea584a7630f7656d2c9362560b427399ca23e
Adil Akbar <adilak389@gmail.com>
2023-08-23 22:32:20 +0400

    Added code

---
+++ custom_module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Custom Module Name',
+    'author': 'Adil Akbar',
+    'category': 'Custom',
+    'summary': 'Custom Odoo Module',
+    'description': 'Custom Odoo Module Tutorial DESCRIPTION....',
+    'version': '16.0.0',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/custom_user_data.xml',
+        'views/menu.xml',
+    ],
+    'installable': True
+}
